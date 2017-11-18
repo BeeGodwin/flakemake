f6623fc6c8553a236679e09e2da559f987c4104e
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2017-11-18 12:58:08 +0000

    enabling travis

--- keytest.py
+++ keytest.py
@@ -2,13 +2,11 @@ import pygame
 import sys
 
 pygame.display.init()
-screen = pygame.display.set_mode ( ( 320 , 240 ) )
-index = 0
+screen = pygame.display.set_mode((320, 240))
 
-while 1:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             print(event.key)
-        index += 1
