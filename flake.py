97068c33c43d7b6846912f459348284a273d8a55
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2017-11-19 20:17:10 +0000

    unit test, pseudocode for get_angles

--- flake.py
+++ flake.py
@@ -1,4 +1,6 @@
 '''FlakeMake 0.01a'''
+import random
+import math
 
 class Branch:
     '''recursively constructs one arm of a flake. Seed the RNG before you create
@@ -9,13 +11,13 @@ class Branch:
         self.leng = leng # length in pixels
         self.angles = self.get_angles(n)
 
-        for node in self.place_nodes(dens, prob, leng):
-            if self.rand_place_branch():
-                self.place_branch(node)
+        # for node in self.place_nodes(dens, prob, leng):
+        #     if self.rand_place_branch():
+        #         self.place_branch(node)
 
     def place_nodes(self, dens, prob, leng):
         '''returns a list of (x, y) tuples of points along this branch.'''
-        return []
+        pass
 
     def rand_place_branch(self):
         '''uses self.p to decide whether or not to place branches.
@@ -29,11 +31,15 @@ class Branch:
     def get_angles(self, n):
         '''returns a list of tuples of (x, y) tuples, describing normalised
         vectors of the angles that the branches off this branch will produce.'''
+        # look at vec to get an angle in degrees using arcsin: (0, -1) should be vertical.
+        # then work out how many angle pairs there are using n.
+        # having worked them out in degrees, convert them back into vectors
+        # return them as a list of pairs of tuples
         pass
 
-def main():
-    branch = Branch(ori=(0, 0), vec=(-1, 0), leng=200, n=6, dens=10, prob=1)
-    print(branch)
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     branch = Branch(ori=(0, 0), vec=(-1, 0), leng=200, n=6, dens=10, prob=1)
+#     print(branch)
+#
+# if __name__ == '__main__':
+#     main()
--- test_flake.py
+++ test_flake.py
@@ -1,4 +1,16 @@
-import flake
+from flake import Branch
+import random, math
 
-def test_nothing():
-    assert 2 + 2 == 4
+def test_create_nodes():
+    pass
+
+def test_rand_place_branch():
+    pass
+
+def test_place_branch():
+    pass
+
+def test_get_angles():
+    branch = Branch(ori=(0,0), vec=(0, -1), leng=100, n=4, dens=1, prob=1)
+    branch.get_angles(4)
+    assert branch.angles[0] == [(-1, 0), (1, 0)]
