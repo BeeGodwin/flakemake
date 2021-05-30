5691d53a98bbae33e712c42e5a4978131c122ce7
Bee Godwin <bee.godwin@bbc.co.uk>
2021-05-30 17:52:16 +0100

    refactor for legibility

--- flake.py
+++ flake.py
@@ -1,42 +1,53 @@
-'''FlakeMake 0.01a'''
+"""FlakeMake 0.01a"""
 import random
 import math
 import numpy
 
+
+def polar(vec):
+    """given a normalised (x, y) vector, return its angle (with 0 being
+    considered vertical, on a scale 0-359.99.)"""
+    phi = math.degrees(numpy.arctan2(vec[0], -vec[1]))
+    if vec[0] < 0:
+        phi += 360
+    return round(phi, 3)
+
+
 class Branch:
-    '''recursively constructs one arm of a flake. Seed the RNG before you create
-    the object if you want branches that look alike.'''
+    """recursively constructs one arm of a flake. Seed the RNG before you create
+    the object if you want branches that look alike."""
 
     def __init__(self, ori=(0, 0), vec=(0, -1), leng=10, n=4, dens=10, prob=1):
-        self.ori = ori # (x, y) tuple describing pt on scr
-        self.vec = vec # (x, y) tuple describing norm vector
-        self.leng = leng # length in pixels
-        self.dens = dens # nodes are this far apart
-        self.prob = prob # 0-1 probability of nodes
-        self.n = n # sidedness of snowflake. should this be a class var?
-        self.angles = self.get_angles(n) # list of pairs of (x, y) norm vectors
-        self.nodes = self.place_nodes() # list of (x, y) points
-        self.branches = [] # list of lists of Branch objects
+        self.ori = ori  # (x, y) tuple describing pt on scr
+        self.vec = vec  # (x, y) tuple describing normalised vector
+        self.leng = leng  # length in pixels
+        self.dens = dens  # nodes are this far apart
+        self.prob = prob  # 0-1 probability of nodes
+        self.n = n  # sidedness of snowflake.
+        self.angles = self.get_angles(n)  # list of pairs of (x, y) norm vectors
+        self.nodes = self.place_nodes()  # list of (x, y) points
+        self.branches = []  # list of lists of Branch objects
 
         for node in self.nodes:
-           self.branches.append(self.place_branches(node))
+            self.branches.append(self.place_branches(node))
 
     def place_nodes(self):
-        '''returns a list of (x, y) tuples of points along this branch. dens says
+        """returns a list of (x, y) tuples of points along this branch. dens says
         how many pixels apart nodes should be. prob is the likelihood of a given
-        node being added to the returned list.'''
-        #print('origin {}, vec {}, len {}, density {}, prob {}.'.format(self.ori, self.vec, self.leng, self.dens, self.prob))
+        node being added to the returned list."""
+        # print('origin {}, vec {}, len {}, density {}, prob {}.'.format(self.ori, self.vec, self.leng, self.dens,
+        # self.prob))
         nodes = []
         x_gap, y_gap = self.vec[0] * self.dens, self.vec[1] * self.dens
         node = (self.ori[0] + x_gap, self.ori[1] + y_gap)
         for i in range(int((self.leng // self.dens)) - 1):
-            if (self.choose()):
+            if self.choose():
                 nodes.append(node)
             node = (node[0] + x_gap, node[1] + y_gap)
         return nodes
 
     def choose(self):
-        '''uses self.prob to choose true/false.'''
+        """uses self.prob to choose true/false."""
         if self.leng < self.dens:
             return False
         if random.random() < self.prob:
@@ -45,8 +56,8 @@ class Branch:
             return False
 
     def place_branches(self, node):
-        '''instantiates a pair(s) of new branches at the given node, and returns
-        a list of pairs of branches.'''
+        """instantiates a pair(s) of new branches at the given node, and returns
+        a list of pairs of branches."""
         branches = []
         if len(self.angles) == 1:
             branches = self.branch_gen(node, self.angles[0])
@@ -57,45 +68,37 @@ class Branch:
         return branches
 
     def branch_gen(self, node, angles):
-        '''parameterizes & instantiates one pair of new branches.'''
+        """parameterizes & instantiates one pair of new branches."""
         sd = random.randrange(random.getrandbits(32))
         l = self.leng - self.dens
         d = self.dens
         if self.choose():
             l /= 2
             d /= 2
-        
+
         random.seed(sd)
         branch_a = Branch(ori=node, vec=angles[0], leng=l, prob=self.prob, dens=d, n=self.n)
-        #print('New branch at {} with vec {} and leng {}'.format(branch_a.ori, branch_a.vec, branch_a.leng))
+        # print('New branch at {} with vec {} and leng {}'.format(branch_a.ori, branch_a.vec, branch_a.leng))
         random.seed(sd)
         branch_b = Branch(ori=node, vec=angles[1], leng=l, prob=self.prob, dens=d, n=self.n)
-        #print('New branch at {} with vec {} and leng {}'.format(branch_b.ori, branch_b.vec, branch_b.leng))
+        # print('New branch at {} with vec {} and leng {}'.format(branch_b.ori, branch_b.vec, branch_b.leng))
         return [branch_a, branch_b]
 
     def get_angles(self, n):
-        '''returns a list of tuples of (x, y) tuples, describing normalised
-        vectors of the angles that the branches off this branch will produce.'''
-        my_angle = self.polar(self.vec)
+        """returns a list of tuples of (x, y) tuples, describing normalised
+        vectors of the angles that the branches off this branch will produce."""
+        my_angle = polar(self.vec)
         pairs = (n - 1) // 2
         angles = []
-        angle_inc = 360 / n
+        theta = 360 / n
         for i in range(pairs):
-            pair = [my_angle + angle_inc * (i + 1), my_angle - angle_inc * (i + 1)]
+            pair = [my_angle + theta * (i + 1), my_angle - theta * (i + 1)]
             angles.append([self.cartesian(pair[0]), self.cartesian(pair[1])])
         return angles
 
-    def polar(self, vec):
-        '''given a normalised (x, y) vector, return its angle (with 0 being
-        considered vertical, on a scale 0-359.99.)'''
-        phi = math.degrees(numpy.arctan2(vec[0], -vec[1]))
-        if vec[0] < 0:
-            phi += 360
-        return round(phi, 3)
-
     def cartesian(self, phi):
-        '''given an angle phi, return a normalised (x, y) vector as a tuple of
-        floats.'''
+        """given an angle phi, return a normalised (x, y) vector as a tuple of
+        floats."""
         x = round(math.sin(math.radians(phi)), 3)
         y = -round(math.cos(math.radians(phi)), 3)
-        return (x, y)
+        return x, y
--- flakemake.py
+++ flakemake.py
@@ -1,23 +1,29 @@
-import pygame, sys, math
-from pygame.locals import *
-from flake import Branch
 import random
 
-def draw_branch(branch, DSURF, weight):
+import math
+import pygame
+import sys
+from pygame.locals import *
+
+from flake import Branch
+
+
+def draw_branch(branch, draw_surface, weight):
     dest_x = branch.ori[0] + (branch.vec[0] * branch.leng)
     dest_y = branch.ori[1] + (branch.vec[1] * branch.leng)
-    pygame.draw.line(DSURF, (255, 255, 255), branch.ori, (dest_x, dest_y), weight)
+    pygame.draw.line(draw_surface, (255, 255, 255), branch.ori, (dest_x, dest_y), weight)
     for pair in branch.branches:
         for child_branch in pair:
-            draw_branch(child_branch, DSURF, weight - 2)
+            draw_branch(child_branch, draw_surface, weight - 2)
+
 
 def main():
     pygame.init()
-    DSURF = pygame.display.set_mode((1024, 768))
+    draw_surface = pygame.display.set_mode((1024, 768))
     pygame.display.set_caption('FlakeMake 0.01a')
-    ORIGIN = (512, 384)
-    n, p, l, d = 6, 0.2, 200, 40
-    new_flake(DSURF, ORIGIN, l, n, 40, p)
+    origin = (512, 384)
+    branches, probability, length, density = 6, 0.2, 200, 40
+    new_flake(draw_surface, origin, length, branches, density, probability)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -25,31 +31,32 @@ def main():
                 sys.exit()
             if event.type == KEYDOWN:
                 if event.key == 32:
-                    new_flake(DSURF, ORIGIN, l, n, d, p)
-                elif event.key == 97: # a
-                    n += 1
-                elif event.key == 100: # d
-                    n -= 1
-                elif event.key == 119: # w
-                    p += 0.05
-                    p = min(p, 0.95)
-                elif event.key == 115: # s
-                    p -= 0.05
-                    p = max(p, 0.05)
-                elif event.key == 101: # e
-                    l += 10
-                    l = min(l, 350)
-                    d += 2
-                    d = min(d, 70)
-                elif event.key == 113: # q
-                    l -= 10
-                    l = max(l, 80)
-                    d -= 2
-                    d = max(d, 16)
+                    new_flake(draw_surface, origin, length, branches, density, probability)
+                elif event.key == 97:  # a
+                    branches += 1
+                elif event.key == 100:  # d
+                    branches -= 1
+                elif event.key == 119:  # w
+                    probability += 0.05
+                    probability = min(probability, 0.95)
+                elif event.key == 115:  # s
+                    probability -= 0.05
+                    probability = max(probability, 0.05)
+                elif event.key == 101:  # e
+                    length += 10
+                    length = min(length, 350)
+                    density += 2
+                    density = min(density, 70)
+                elif event.key == 113:  # q
+                    length -= 10
+                    length = max(length, 80)
+                    density -= 2
+                    density = max(density, 16)
         pygame.display.update()
 
-def new_flake(DSURF, ORIGIN, l, sides, d, p):
-    DSURF.fill((0,0,0))
+
+def new_flake(draw_surface, origin, length, sides, density, probability):
+    draw_surface.fill((0, 0, 0))
     angle = 360 / sides
     seed = random.randrange(random.getrandbits(32))
 
@@ -57,18 +64,20 @@ def new_flake(DSURF, ORIGIN, l, sides, d, p):
         random.seed(seed)
         branch_angle = angle * i
         vec_x, vec_y = math.sin(math.radians(branch_angle)), math.cos(math.radians(branch_angle))
-        branch = Branch(ori=ORIGIN, vec=(vec_x, vec_y), leng=l, n=sides, dens=d, prob=p)
+        branch = Branch(ori=origin, vec=(vec_x, vec_y), leng=length, n=sides, dens=density, prob=probability)
         depth = flake_depth(branch, 0)
-        if depth < 3: # NOTE get rid of magic numbers.
-            new_flake(DSURF, ORIGIN, l, sides, d, p)
+        if depth < 3:
+            new_flake(draw_surface, origin, length, sides, density, probability)
             break
-        draw_branch(branch, DSURF, 9)
+        draw_branch(branch, draw_surface, 9)
+
 
 def flake_depth(branch, count):
     if len(branch.branches) == 0:
         return count
     count += 1
     return flake_depth(branch.branches[0][0], count)
+
 
 if __name__ == '__main__':
     main()
